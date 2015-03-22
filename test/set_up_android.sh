0f3eb1112b691e87ac8bc30a7de9df08e76987c9
Pier Paolo Ramon <ramonpierre@gmail.com>
2015-03-22 02:28:14 +0100

    Add zipalign to installed components

--- test/set_up_android.sh
+++ test/set_up_android.sh
@@ -4,7 +4,7 @@ echo "# Executing https://raw.github.com/embarkmobile/android-sdk-installer"
 echo ""
 
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | \
-  bash /dev/stdin --install=build-tools-18.1.0,android-17,sys-img-armeabi-v7a-android-17 && \
+  bash /dev/stdin --install=zipalign,build-tools-18.1.0,android-17,sys-img-armeabi-v7a-android-17 && \
   source ~/.android-sdk-installer/env
 
 echo ""
